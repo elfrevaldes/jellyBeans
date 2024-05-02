9febce3135f6b525a89d72ef753f8d706d6afd6c
Elfre Valdes <elfre.valdes@gmail.com>
2024-05-02 15:29:36 -0600

    Name change to app component for better cohesion

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-home',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class AppComponent {
-  title = 'JellyBeans';
+export class HomeComponent {
+  title = 'Jelly Beans';
 }
